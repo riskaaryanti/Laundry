796fd1c6d878301cf45f32c8ad856cfd6b44fa0d
riskaaryanti <79629570+riskaaryanti@users.noreply.github.com>
2021-02-27 10:55:58 +0700

    Add files via upload

--- data.php
+++ data.php
@@ -1,41 +1,36 @@
 <div class='panel panel-border panel-primary'>
                                     <div class='panel-heading'> 
-                                        <h3 class='panel-title'><i class='fa fa-clock-o'></i> Riwayat Transaksi</h3> 
+                                        <h3 class='panel-title'><i class='fa fa-list'></i> Data Pembelian Barang</h3> 
                                     </div>  <div class='panel-body'> 
                                 <table id='datatable' class='table table-hover'>
                                     <thead>
                                         <tr>
 										<th><i class='icon-terminal'></i> No</th>
-										<th><i class='icon-signal'></i> Konsumen</th>
-										<th><i class='icon-terminal'></i> Jenis</th>
-											<th><i class='icon-signal'></i> Tarif</th>
-											<th><i class='icon-signal'></i> Berat</th>
-											<th><i class='icon-signal'></i> Tanggal Transaksi</th>
-											<th><i class='icon-signal'></i> Tanggal Ambil</th>
-
-											<th><i class='icon-signal'></i> Kwitansi</th>
+                                            <th><i class='icon-terminal'></i> Nama Barang</th>
+                                            <th><i class='icon-time'></i> Jumlah</th>
+											<th><i class='icon-signal'></i> Total Harga</th>
+											<th><i class='icon-signal'></i> Supplier</th>
+											<th><i class='icon-signal'></i> Tanggal Pembelian</th>
                                         </tr>
                                     </thead>
                                     <tbody>
 									<?php
 							$i=1;
-							$tp=mysql_query("SELECT * FROM transaksi WHERE pengguna='$usr' ORDER BY tgl_transaksi");
+							$tp=mysql_query("SELECT * FROM pembelian ORDER BY no ");
 							while($r=mysql_fetch_array($tp)){
 							?>
 							<tr>
 							 <td><?php echo $i;?></td>
-							 
-                                    <td><?php echo $r['konsumen'];?></td>
-                                    <td><?php echo $r['jenis'];?></td>
-									<td><?php echo'Rp.' . number_format( $r['tarif'], 0 , '' , '.' ) . ',-'?></td>
-									<td><?php echo $r['berat']?> Kg</td>
-									<td><?php echo TanggalIndo($r['tgl_transaksi']);?></td>
-									<td><?php echo TanggalIndo($r['tgl_ambil']);?></td>
-									<td><a href="transaksi/kwitansi.php?id=<?php echo $r['id'];?>" target="_blank">Lihat Kwitansi</a></td>
+                                    <td><?php echo $r['barang'];?></td>
+									<td><?php echo $r['totali'];?></td>
+									<td><?php echo'Rp.'.number_format($r['totalh'],0,'','.').',-'?></td>
+									<td><?php echo $r['supplier'];?></td>
+									<td><?php echo TanggalIndo($r['tgl']);?></td>
                                     
 							</tr>
 							<?php $i=$i+1;?>
 							<?php } ?>
+								
 							</tbody>
                         </table>
                                      </div><!-- /.box-body -->
